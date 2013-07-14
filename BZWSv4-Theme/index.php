c7aa22b2f8d1328a881088b730020fa648a6b865
David Hobbs <davidhobbs83@gmail.com>
2013-07-14 15:44:59 +1000

    index.php

---
+++ BZWSv4-Theme/index.php
@@ -0,0 +1,15 @@
+
+<?php include('inc/html_header.php'); ?>
+<?php include('inc/the_header.php'); ?>
+<?php include('inc/the_nav.php'); ?>
+
+
+	<div class="row">
+		<div class="large-12 columns">
+			<h2><?php the_title(); ?></h2>
+			
+		</div>
+	</div>
+
+<?php include('inc/the_footer.php'); ?>
+<?php include('inc/html_footer.php'); ?>
